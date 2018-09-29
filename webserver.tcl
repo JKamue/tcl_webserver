12ae7cc38e5f47a87c8f0e9339238fd53a8d920b
JKamue <laptop@jkamue.de>
2018-09-29 11:54:31 +0200

    v.1.1

--- webserver.tcl
+++ webserver.tcl
@@ -1,7 +1,10 @@
 package require Tk
+package require BWidget
+
 set root "c:/html"
 set default "index.html"
 set port 82
+
 
 wm title . "JKamue Webserver"
 
@@ -19,10 +22,37 @@ grid [ttk::label .c.porttxt -text "Port"] -column 5 -row 1 -sticky w
 
 grid [ttk::label .c.status -text "Status"] -column 6 -row 2 -sticky w
 
-grid [ttk::button .c.start -text "Start" -command {set ::until_time_to_start "now, please"}] -column 2 -row 2 -sticky w
-grid [ttk::button .c.stop -text "Stop" -style "TButton" -command {set ::until_time_to_stop "now, please"}] -column 4 -row 2 -sticky w
+grid [ttk::button .c.start -text "Start" -command {server $port}] -column 2 -row 2 -sticky w
+grid [ttk::button .c.stop -text "Stop" -style "TButton" -command {stopServer $port}] -column 4 -row 2 -sticky w
+	
+listbox .lb -selectmode multiple -height 4
+scrollbar .sb -command [list .lb yview]
+.lb configure -yscrollcommand [list .sb set]
+.lb insert 0 "Logs"
+grid .lb .sb -sticky news
+grid columnconfigure . 0 -weight 1
+grid rowconfigure . 0 -weight 1
+.lb itemconfigure 0 -foreground black
+
+set lb_count 1
 
 foreach w [winfo children .c] {grid configure $w -padx 5 -pady 5}
+
+proc server {port} {
+	.c.status configure -foreground "green"
+	.c.status configure -text "Running"
+	puts "\n\nActivatet\n"
+	uplevel set test "test"
+	uplevel set server [socket -server answer $port]
+}
+
+proc stopServer {port} {
+	.c.status configure -foreground "red"
+	.c.status configure -text "Stoped"
+	puts "\n\nDeactivatet\n"
+	upvar server server
+	close $server
+}
 
 proc bgerror {trouble} {puts stdout "bgerror: $trouble"}
 
@@ -32,7 +62,7 @@ proc answer {socketChannel addr port2} {
 
 proc readIt {socketChannel addr} {
   set systemTime [clock seconds]
-  puts "[clock format $systemTime -format %H:%M:%S] $addr"
+  
   global root default
   fconfigure $socketChannel -blocking 0
   set gotLine [gets $socketChannel]
@@ -45,6 +75,10 @@ proc readIt {socketChannel addr} {
   if {$last=="/"} then {set shortName $shortName$default }
   set wholeName $root$shortName
 
+  puts "[clock format $systemTime -format %H:%M:%S] $addr"
+  upvar lb_count lb_count
+  .lb insert $lb_count "[clock format $systemTime -format %H:%M:%S] $addr $wholeName"
+  
   if {[catch {set fileChannel [open $wholeName RDONLY] } ]} {
     puts $socketChannel "HTTP/1.0 404 Not found"
     puts $socketChannel ""
@@ -53,31 +87,19 @@ proc readIt {socketChannel addr} {
     puts $socketChannel "The URL you requested does not exist on this site."
     puts $socketChannel "</center></body></html>"
     close $socketChannel
-	set ::until_time_to_stop "now, please"
+	.lb itemconfigure $lb_count -foreground orange
   } else {
     fconfigure $fileChannel -translation binary
     fconfigure $socketChannel -translation binary -buffering full
     puts $socketChannel "HTTP/1.0 200 OK"
     puts $socketChannel ""
     fcopy $fileChannel $socketChannel -command [list done $fileChannel $socketChannel]
+	.lb itemconfigure $lb_count -foreground green
   }
-
+   set lb_count [expr {$lb_count + 1}] 
 }
 
 proc done {inChan outChan args} {
   close $inChan
   close $outChan
-}
-
-while {1} {
-.c.status configure -foreground "red"
-.c.status configure -text "Stopped"
-vwait until_time_to_start
-.c.status configure -foreground "green"
-.c.status configure -text "Running"
-puts "\n\nActivatet\n";
-set server [socket -server answer $port]
-vwait until_time_to_stop
-puts "\n\nDeactivaten\n";
-close $server
 }
